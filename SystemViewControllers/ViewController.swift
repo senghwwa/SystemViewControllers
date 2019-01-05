eb8e86aa2c5d0ef404ef253c2db45865acb0524e
Seng Hwwa <senghwwa@zana-girl.local>
2019-01-05 15:23:27 +0800

    Initial Commit

---
+++ SystemViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Seng Hwwa on 05/01/2019.
+//  Copyright © 2019 senghwabeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
